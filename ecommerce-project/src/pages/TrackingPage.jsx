95326e4dc567ad8e3c131b6b9e01247cc77ef086
Esma <asmaelmokretar19@gmail.com>
2025-09-30 06:25:05 +0100

    Fix checkout links

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -30,7 +30,7 @@ export function TrackingPage() {
                         <span className="orders-text">Orders</span>
                     </a>
 
-                    <a className="cart-link header-link" href="/ckechout">
+                    <a className="cart-link header-link" href="/checkout">
                         <img className="cart-icon" src="images/icons/cart-icon.png" />
                         <div className="cart-quantity">3</div>
                         <div className="cart-text">Cart</div>
